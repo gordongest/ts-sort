4075b36192f0b6dabd593c031a1fa59ff4d69cdf
gordongest <gordon.gest@gmail.com>
2021-03-25 11:01:08 -0600

    extracted Sorter class into its own file,  built rudimentary logic

--- build/index.js
+++ build/index.js
@@ -1,24 +1,6 @@
 "use strict";
-var Sorter = /** @class */ (function () {
-    // collection: number[];
-    function Sorter(collection) {
-        this.collection = collection;
-        // this.collection = collection;
-    }
-    Sorter.prototype.bubbleSort = function () {
-        var length = this.collection.length;
-        for (var i = 0; i < length; i++) {
-            for (var j = 0; j < length - i - 1; j++) {
-                if (this.collection[j] > this.collection[j + 1]) {
-                    var temp = this.collection[j];
-                    this.collection[j] = this.collection[j + 1];
-                    this.collection[j + 1] = temp;
-                }
-            }
-        }
-    };
-    return Sorter;
-}());
-var sorter = new Sorter([4, 3, 10, -1]);
+Object.defineProperty(exports, "__esModule", { value: true });
+var Sorter_1 = require("./Sorter");
+var sorter = new Sorter_1.Sorter([4, 3, 10, -1]);
 sorter.bubbleSort();
 console.log(sorter.collection);
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,4 @@
-class Sorter {
-  // collection: number[];
-
-  constructor(public collection: number[]) {
-    // this.collection = collection;
-  }
-
-  bubbleSort(): void {
-    const { length } = this.collection;
-
-    for (let i = 0; i < length; i++) {
-      for (let j = 0; j < length - i - 1; j++) {
-        if (this.collection[j] > this.collection[j + 1]) {
-          let temp: number = this.collection[j];
-          this.collection[j] = this.collection[j + 1];
-          this.collection[j + 1] = temp;
-        }
-      }
-    }
-  }
-}
+import { Sorter } from './Sorter'
 
 const sorter = new Sorter([4, 3, 10, -1]);
 sorter.bubbleSort();
