0b52ea1e2eb612254312252877eef74500612be3
gordongest <gordon.gest@gmail.com>
2021-03-25 09:49:16 -0600

    initializing repo

---
+++ build/index.js
@@ -0,0 +1,6 @@
+"use strict";
+console.log('hello, there!');
+var logSomething = function (a) {
+    console.log(a);
+};
+logSomething('something');
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+console.log('hello, there!');
+
+const logSomething = (a: string): void => {
+  console.log(a);
+};
+
+logSomething('something');
